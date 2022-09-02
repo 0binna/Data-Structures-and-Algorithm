75f24f5b61b68aedb9f6019ceb87cb729e8127db
Obinna <obi.udeogu@gmail.com>
2022-09-02 18:12:16 -0400

    Add LeetCode 3

---
+++ 0003_Longest_Substring_Without_Repeating_Characters.py
@@ -0,0 +1,25 @@
+string = "agghj"
+
+def lengthOfLongestSubstring(s):
+    if len(s) <= 1: 
+        return len(s)
+    
+    seen = {}
+    left, longest, right = 0, 0, 0
+    
+    while right < len(s):
+        currentChar = s[right]
+        if currentChar in seen.keys():
+            previouslySeenChar = seen[currentChar]
+            
+            if previouslySeenChar >= left:
+                left = previouslySeenChar + 1
+        
+        seen[currentChar] = right
+        
+        longest = max(longest, (right - left + 1))
+    
+        right += 1
+    return longest
+
+print(lengthOfLongestSubstring(string))
